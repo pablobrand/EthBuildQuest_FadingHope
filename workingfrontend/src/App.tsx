ea3165794c3e525eee577a2718bbf327529e0af7
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-03-10 08:25:34 +0530

    updated

--- workingfrontend/src/App.tsx
+++ workingfrontend/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, Typography, Button } from "antd";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import abi from "../src/contracts/sla.json";
 import {
   Footer,
   Blog,
@@ -12,9 +14,20 @@ import {
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
 import './App.css';
-import { useMoralis } from "react-moralis";
+
 
 function App() {
+
+  const { runContractFunction, isLoading } = useWeb3Contract({
+    functionName: "freeMintURI",
+    abi,
+    contractAddress: "0xC34541DEec223F4a24bD7Eeda28D56cA16c927fd",
+    params: {
+      account:"",
+      kingdomName: document.getElementsByClassName('kingdomName'),
+      uri: "pldosksmmm",
+    },
+  });
 
     const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
 
@@ -61,10 +74,41 @@ function App() {
         <Possibility />
         <CTA />
         <Blog />
+          <div style={{ display: "flex" }}>
+      <Card
+        bordered={false}
+        style={{
+          width: 600,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography.Title level={3}>NFT Minter</Typography.Title>
+        <img
+          src="https://ipfs.moralis.io:2053/ipfs/QmebxzVBtcEznrZgSUxorrdL8Q1XEbiyRaGxHUuwWUoF1o/images/0.png"
+          alt="Test"
+          style={{ marginBottom: "2rem" }}
+        />
+        <Button
+          type="primary"
+          shape="round"
+          size="large"
+          style={{ width: "100%" }}
+          loading={isLoading}
+          onClick={() => runContractFunction()}
+        >
+          MINT
+        </Button>
+        <input id="kingdomName" type="text" className="form-control" placeholder="Kingdom Name" aria-label="kingdomName" aria-describedby="basic-addon1"/>
+        <input id="URI" type="text" className="form-control" placeholder="URI" aria-label="URI" aria-describedby="basic-addon1"/>
+      </Card>
         <Footer />
       </div>
-    </div>
+      </div>
+      </div>
   );
 }
 
-export default App;
+  export default App;
---
+++ workingfrontend/src/minter.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Card, Typography, Button } from "antd";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { abi } from "../contracts/sla.json";
+
+
+
+
+export default function QuickStart() {
+  const { account } = useMoralis();
+  const { runContractFunction, isLoading } = useWeb3Contract({
+    functionName: "freeMintURI",
+    abi,
+    contractAddress: "0xC34541DEec223F4a24bD7Eeda28D56cA16c927fd",
+    params: {
+      account,
+      kingdomName: document.getElementById('kingdomName').setAttribute("disabled", null),
+      uri: "pldosksmmm",
+    },
+  });
+
+
+
+
+
+
+
+  return (
+    <div style={{ display: "flex" }}>
+      <Card
+        bordered={false}
+        style={{
+          width: 600,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        <Typography.Title level={3}>NFT Minter</Typography.Title>
+        <img
+          src="https://ipfs.moralis.io:2053/ipfs/QmebxzVBtcEznrZgSUxorrdL8Q1XEbiyRaGxHUuwWUoF1o/images/0.png"
+          alt="Test"
+          style={{ marginBottom: "2rem" }}
+        />
+        <Button
+          type="primary"
+          shape="round"
+          size="large"
+          style={{ width: "100%" }}
+          loading={isLoading}
+          onClick={() => runContractFunction()}
+        >
+          MINT
+        </Button>
+        <input id="kingdomName" type="text" class="form-control" placeholder="Kingdom Name" aria-label="kingdomName" aria-describedby="basic-addon1"/>
+        <input id="URI" type="text" class="form-control" placeholder="URI" aria-label="URI" aria-describedby="basic-addon1"/>
+      </Card>
+    </div>
+  );
+}
