9dee3b57c74c7cde2985ab3ab9e66dc9941edddb
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-03-10 05:32:49 +0530

    Revert "Add files via upload"
    This reverts commit bcee34b3e177718e8a506571996eb7a25db9a2af.

--- src/components/brand/imports.js
+++
@@ -1,8 +0,0 @@
-import pinata from '../../assets/pinata.png';
-import valist from '../../assets/valist.jpg';
-import moralis from '../../assets/moralis.jpg';
-import ipfs from '../../assets/ipfs.png';
-import chainlink from '../../assets/chainlink.jpg';
-import immutable from '../../assets/immutable.jpg';
-
-export { pinata, valist, moralis, ipfs, chainlink, immutable };
